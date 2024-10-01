cd53704ead69525555f6de5c70ef6c58d3ba0236
kmc198989 <kmc198989@naver.com>
2024-10-01 10:41:15 +0900

    feat: jwt refresh token, 블랙리스트 기능 구현

--- auth/src/main/java/com/tradingtrends/auth/application/AuthService.java
+++ auth/src/main/java/com/tradingtrends/auth/application/AuthService.java
@@ -1,6 +1,5 @@
 package com.tradingtrends.auth.application;
 
-import com.tradingtrends.auth.application.dto.AuthResponse;
 import com.tradingtrends.auth.infrastructure.client.UserClient;
 import com.tradingtrends.auth.infrastructure.client.UserResponse;
 import feign.FeignException;
@@ -8,17 +7,21 @@ import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class AuthService {
 
     private final UserClient userClient;
     private final PasswordEncoder passwordEncoder;
+    private final SecretKey secretKey;
+    private final RedisTemplate<String, Object> redisTemplate;
 
     @Value("${spring.application.name}")
     private String issuer;
@@ -29,18 +32,25 @@ public class AuthService {
     @Value("${service.jwt.refresh-expiration}")
     private Long refreshExpiration;
 
-    private final SecretKey secretKey;
-
-    public AuthService(UserClient userClient, @Value("${service.jwt.secret-key}") String secretKey, PasswordEncoder passwordEncoder) {
+    public AuthService(UserClient userClient, @Value("${service.jwt.secret-key}") String secretKey, PasswordEncoder passwordEncoder, RedisTemplate<String, Object> redisTemplate) {
         this.userClient = userClient;
         this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(secretKey));
         this.passwordEncoder = passwordEncoder;
+        this.redisTemplate = redisTemplate;
     }
 
-    public AuthResponse createAccessToken(final String userId, final String rawPassword) {
+    // 로그인 시 Access Token과 Refresh Token을 생성하고 반환 (AccessToken은 응답, RefreshToken은 쿠키로 처리)
+    public String createAccessToken(final String userId, final String rawPassword) {
         UserResponse user = retrieveUser(userId);
         validatePassword(rawPassword, user.getPassword());
-        return generateAccessToken(user);
+
+        // AccessToken만 생성하여 반환 (RefreshToken은 별도로 처리)
+        return generateToken(user, accessExpiration);
+    }
+
+    public String createRefreshToken(final String userId) {
+        UserResponse user = retrieveUser(userId);
+        return generateToken(user, refreshExpiration);
     }
 
     private UserResponse retrieveUser(final String userId) throws RuntimeException {
@@ -59,53 +69,52 @@ public class AuthService {
         }
     }
 
-    private AuthResponse generateAccessToken(final UserResponse user) {
-        return AuthResponse.of(Jwts.builder()
+    // AccessToken 및 RefreshToken 생성 메서드 통합
+    private String generateToken(final UserResponse user, Long expirationTime) {
+        return Jwts.builder()
                 .claim("user_id", user.getUserId())
                 .claim("email", user.getEmail())
                 .claim("role", user.getRole())
                 .issuer(issuer)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + accessExpiration))
-                .signWith(secretKey, SignatureAlgorithm.HS512)
-                .compact());
+                .expiration((new Date(System.currentTimeMillis() + expirationTime)))
+                .signWith(secretKey, Jwts.SIG.HS512)
+                .compact();
     }
 
-    public AuthResponse createRefreshToken(final String userId) {
-        UserResponse user = retrieveUser(userId);
-        return generateRefreshToken(user);
-    }
+    // AccessToken 재발행 (RefreshToken은 갱신하지 않음)
+    public String refreshAccessToken(final String refreshToken) {
+        // 1. RefreshToken이 블랙리스트에 등록되어 있는지 확인
+        if (isTokenInBlacklist(refreshToken)) {
+            throw new RuntimeException("유효하지 않은 Refresh Token입니다. 해당 토큰은 이미 로그아웃 처리되었습니다.");
+        }
 
-    private AuthResponse generateRefreshToken(final UserResponse user) {
-        return AuthResponse.of(Jwts.builder()
-                .claim("user_id", user.getUserId())
-                .claim("email", user.getEmail())
-                .claim("role", user.getRole())
-                .issuer(issuer)
-                .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + refreshExpiration))
-                .signWith(secretKey, SignatureAlgorithm.HS512)
-                .compact());
-    }
+        try {
+            // 2. RefreshToken 검증
+            Jws<Claims> claims = parseToken(refreshToken);
+            String userId = claims.getBody().get("user_id", String.class);
+            UserResponse user = retrieveUser(userId);
 
-    public AuthResponse refreshAccessToken(final String refreshToken) {
-        Jws<Claims> claims = parseToken(refreshToken);
-        String userId = claims.getBody().get("user_id", String.class);
-        UserResponse user = retrieveUser(userId);
-        return generateAccessToken(user);
+            // 3. 새로운 AccessToken 발급
+            return generateToken(user, accessExpiration);  // 새로운 AccessToken만 생성하여 반환
+        } catch (ExpiredJwtException e) {
+            throw new RuntimeException("Refresh Token이 만료되었습니다.");
+        } catch (JwtException e) {
+            throw new RuntimeException("유효하지 않은 Refresh Token입니다.");
+        }
     }
-
     private Jws<Claims> parseToken(final String token) {
         try {
-            return Jwts.parser()  // parser() 대신 parserBuilder() 사용
-                    .setSigningKey(secretKey)  // 시그니처 키 설정
-                    .build()  // 빌드 메서드로 파서 인스턴스 생성
-                    .parseClaimsJws(token);  // 토큰을 파싱
+            return Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(token);
+        } catch (ExpiredJwtException e) {
+            throw new RuntimeException("토큰이 만료되었습니다.");
         } catch (JwtException e) {
             throw new RuntimeException("유효하지 않은 토큰입니다.");
         }
     }
-
 
     public Boolean verifyUser(final String userId) {
         try {
@@ -113,5 +122,28 @@ public class AuthService {
         } catch (Exception e) {
             throw new RuntimeException("유저 검증 중 문제가 발생했습니다: " + e.getMessage());
         }
+    }
+
+    public void logout(String accessToken, String refreshToken) {
+        // Access Token과 Refresh Token을 블랙리스트에 추가
+        addTokenToBlacklist(accessToken);
+        addTokenToBlacklist(refreshToken);
+    }
+
+    private void addTokenToBlacklist(String token) {
+        long expiration = getExpirationFromToken(token);
+        if (expiration > 0) {  // 만료된 토큰은 블랙리스트에 등록하지 않음
+            redisTemplate.opsForValue().set(token, "true", expiration, TimeUnit.MILLISECONDS); // Boolean 대신 String 저장
+        }
+    }
+
+    private boolean isTokenInBlacklist(String token) {
+        // Redis에 저장된 토큰이 있는지 확인
+        return Boolean.TRUE.equals(redisTemplate.hasKey(token));
+    }
+
+    private long getExpirationFromToken(String token) {
+        Jws<Claims> claims = parseToken(token);
+        return claims.getPayload().getExpiration().getTime() - System.currentTimeMillis();
     }
 }
--- auth/src/main/java/com/tradingtrends/auth/presentation/controller/AuthController.java
+++ auth/src/main/java/com/tradingtrends/auth/presentation/controller/AuthController.java
@@ -1,59 +1,84 @@
 package com.tradingtrends.auth.presentation.controller;
 
 import com.tradingtrends.auth.application.AuthService;
-import com.tradingtrends.auth.application.dto.AuthResponse;
 import com.tradingtrends.auth.presentation.request.LoginRequestDto;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     private final AuthService authService;
-    private final String serverPort;
 
-    public AuthController(AuthService authService, @Value("${server.port}") String serverPort) {
+    @Value("${service.jwt.refresh-expiration}")
+    private Long refreshExpiration;  // refresh token 만료 시간
+
+    public AuthController(AuthService authService) {
         this.authService = authService;
-        this.serverPort = serverPort;
     }
 
     // 로그인 API
     @PostMapping("/login")
-    public ResponseEntity<AuthResponse> createAuthenticationToken(@RequestBody LoginRequestDto loginRequestDto) {
-        AuthResponse response = authService.createAccessToken(loginRequestDto.getUserId(), loginRequestDto.getPassword());
-        return createResponse(ResponseEntity.ok(response));
+    public ResponseEntity<Void> createAuthenticationToken(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
+        // 토큰 생성
+        String accessToken = authService.createAccessToken(loginRequestDto.getUserId(), loginRequestDto.getPassword());
+        String refreshToken = authService.createRefreshToken(loginRequestDto.getUserId());
+
+        // refreshToken을 쿠키에 저장
+        Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
+        refreshTokenCookie.setHttpOnly(true);
+        refreshTokenCookie.setSecure(true); // HTTPS를 사용할 때만 설정
+        refreshTokenCookie.setPath("/");
+        refreshTokenCookie.setMaxAge((int) TimeUnit.MILLISECONDS.toSeconds(refreshExpiration)); // 만료시간 설정
+
+        response.addCookie(refreshTokenCookie);
+
+        // accessToken을 헤더에 추가
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + accessToken);
+
+        return new ResponseEntity<>(headers, ResponseEntity.ok().build().getStatusCode());
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(@RequestHeader("Authorization") String accessToken, @CookieValue("refreshToken") String refreshToken, HttpServletResponse response) {
+        authService.logout(accessToken.replace("Bearer ", ""), refreshToken);
+
+        // 쿠키 삭제
+        Cookie deleteCookie = new Cookie("refreshToken", null);
+        deleteCookie.setHttpOnly(true);
+        deleteCookie.setSecure(true);
+        deleteCookie.setPath("/");
+        deleteCookie.setMaxAge(0); // 쿠키 만료
+        response.addCookie(deleteCookie);
+
+        return ResponseEntity.noContent().build();
     }
 
     // userId 존재여부 검증 API
     @GetMapping("/verify")
     public ResponseEntity<Boolean> verifyUser(@RequestParam("user_id") String userId) {
         Boolean response = authService.verifyUser(userId);
-        return createResponse(ResponseEntity.ok(response));
-    }
-
-    // Refresh Token 생성 API
-    @PostMapping("/refresh-token")
-    public ResponseEntity<AuthResponse> createRefreshToken(@RequestParam("user_id") String userId) {
-        AuthResponse response = authService.createRefreshToken(userId);
-        return createResponse(ResponseEntity.ok(response));
+        return ResponseEntity.ok(response);
     }
 
     // Access Token 재발행 API
     @PostMapping("/refresh-access-token")
-    public ResponseEntity<AuthResponse> refreshAccessToken(@RequestHeader("Authorization") String refreshToken) {
-        // "Bearer " 접두사를 제외한 토큰 값만 추출
-        String token = refreshToken.replace("Bearer ", "");
-        AuthResponse response = authService.refreshAccessToken(token);
-        return createResponse(ResponseEntity.ok(response));
-    }
+    public ResponseEntity<Void> refreshAccessToken(@CookieValue("refreshToken") String refreshToken) {
+        // Refresh Token을 이용해 새로운 Access Token 생성
+        String newAccessToken = authService.refreshAccessToken(refreshToken);
 
-    // Response Header에 `Server-Port` 추가해주는 함수
-    private <T> ResponseEntity<T> createResponse(ResponseEntity<T> response) {
-        HttpHeaders headers = HttpHeaders.writableHttpHeaders(response.getHeaders());
-        headers.add("Server-Port", serverPort);
-        return new ResponseEntity<>(response.getBody(), headers, response.getStatusCode());
+        // 새로운 Access Token을 헤더에 추가
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + newAccessToken);
+
+        return new ResponseEntity<>(headers, ResponseEntity.ok().build().getStatusCode());
     }
 }
