f947e8010d24b4e1d2c108f495052407153f6650
yezy <iyeahji@gmail.com>
2024-10-03 23:20:49 +0900

    feat: Redis 설정 및 컨슈밍한 데이터를 Redis에 캐싱
    - RedisConfig: RedisTemplate을 통한 Redis 설정
    - MarketDataConsumer: Kafka로부터 수신한 데이터를 Redis에 저장하도록 로직 구현
      - JSON 데이터를 파싱하여 Redis에 해시 형태로 저장

---
+++ market/src/main/java/com/tradingtrends/market/infrastructure/config/RedisConfig.java
@@ -0,0 +1,24 @@
+package com.tradingtrends.market.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    // RedisTemplate을 통해 Redis와 상호작용
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer()); // Key를 String으로 직렬화
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer()); // Value를 JSON 형태로 직렬화
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return redisTemplate;
+    }
+}
