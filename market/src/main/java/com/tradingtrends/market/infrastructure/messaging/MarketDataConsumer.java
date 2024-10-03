da2b05c0cd8f164b93b9c9c7cf9ad47296442157
Inhee <whdlsgml1599@gmail.com>
2024-10-03 16:33:28 +0900

    refactor : Kafka send, listener 로직 번경 - 데이터 json 직렬화 하여 전송

--- market/src/main/java/com/tradingtrends/market/infrastructure/messaging/MarketDataConsumer.java
+++ market/src/main/java/com/tradingtrends/market/infrastructure/messaging/MarketDataConsumer.java
@@ -1,7 +1,11 @@
 package com.tradingtrends.market.infrastructure.messaging;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.protocol.types.Field.Str;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +15,29 @@ import java.util.Map;
 @Service
 @RequiredArgsConstructor
 public class MarketDataConsumer {
+
+    private final ObjectMapper objectMapper;
+
+    // Kafka 메시지 수신 및 처리
     @KafkaListener(topics = "upbit-data", groupId = "market-group")
-    public void handleCoinEvent(Map<String, Object> message) {
+    public void listener(ConsumerRecord<String, String> data) {
+        log.info("Received message: {}", data.toString());
+
         try {
-            log.info("Consumed message: {}", message);
-        } catch (Exception e) {
-            log.error("Failed to consume message: {}", e.getMessage(), e);
+            // JSON 데이터를 객체로 변환
+            Map<String, String> parsedData = objectMapper.readValue(data.value(), Map.class);
+
+            // 데이터를 처리하거나 필요에 따라 로직 추가
+            log.info("Parsed Data: {}", parsedData);
+
+            // 예: 특정 필드가 있는지 확인하고 처리
+            if (parsedData.containsKey("market")) {
+                String marketCode = (String) parsedData.get("market");
+                log.info("Market Code: {}", marketCode);
+            }
+        } catch (JsonProcessingException e) {
+            // JSON 역직렬화 실패 처리
+            log.error("Failed to deserialize message", e);
         }
     }
 }
