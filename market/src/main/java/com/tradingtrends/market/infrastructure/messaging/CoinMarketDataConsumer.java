33197613696f1c42deb0736de9355be31d578232
Inhee <whdlsgml1599@gmail.com>
2024-10-14 14:29:46 +0900

    fix : git conflict solve

--- market/src/main/java/com/tradingtrends/market/infrastructure/messaging/CoinMarketDataConsumer.java
+++ market/src/main/java/com/tradingtrends/market/infrastructure/messaging/CoinMarketDataConsumer.java
@@ -1,6 +1,5 @@
 package com.tradingtrends.market.infrastructure.messaging;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tradingtrends.market.infrastructure.util.CheckChannelInRedis;
 import lombok.RequiredArgsConstructor;
